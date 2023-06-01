3bccc1e66e824cd6f985d802bf64b5d1e8182790
TechPizza <23627133+TechnologicalPizza@users.noreply.github.com>
2023-06-01 22:26:46 +0200

    Moved files and added WinForm

---
+++ TowerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace TowerDefense
+{
+    public class TowerManager
+    {
+        public List<Tower> towerList = new();
+
+        public void AddTower(Tower tower)
+        {
+            towerList.Add(tower);
+        }
+
+        public void Update(
+            float deltaTime,
+            BulletManager bulletManager,
+            EnemyManager enemyManager)
+        {
+            foreach (Tower tower in towerList)
+            {
+                tower.Update(deltaTime, bulletManager, enemyManager);
+            }
+        }
+    }
+}
